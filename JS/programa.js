c5598e3844663f9581eead130459d53f9e622bc3
Anairam Falcón <ggf73659@udelasalle.edu.mx>
2021-08-27 16:09:55 -0500

    Menú comida actualizado

--- JS/programa.js
+++ JS/programa.js
@@ -1,77 +1,73 @@
-document.write("<h1>Programa restaurante, My Little Pony</h1>")
+document.write("<h1>Restaurante My Little Pony</h1>")
 
-console.log("Bienvenidxs al restaurante, ¿qué van a querer de comer?")
-console.log("Platillos (seleccione una letra):")
-console.log("a) Nachos $30")
-console.log("b) Pizza $120")
-console.log("c) Tacos $10")
+var comida = ""
+var bebidaop = ""
+var cantcomida = 0
+var cantbebida = 0
+var totalcomida = 0
 
-var op="a"
-
-switch(op){
-    case "a":
-        var plato=30;
-        console.log("Escogió nachos $30")
+comida = prompt("Menú Platillos \n Selecione la que sea de su agrado \n1) Hamburguesa $40 \n2) Hotdog $30 \n3) Papitas $25")
+switch (comida) {
+    case "1":
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida = parseInt(cantcomida);
+        comidaop = "Hamburguesa"
+        totalcomida += 40 * cantcomida
         break;
-    case "b":
-        var plato=120;
-        console.log("Escogió pizza $120")
+    case "2":
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida = parseInt(cantcomida);
+        totalcomida += 30 * cantcomida
+        comidaop = "Hot Dog"
         break;
-    case "c":
-        var plato=10;
-        console.log("Escogió tacos $10")
+    case "3":
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida = parseInt(cantcomida);
+        totalcomida += 25 * cantcomida
+        comidaop = "Papitas"
         break;
     default:
-        console.log("Opción inválida")
+        alert("Opción inexistente. Refresque la pantalla para volver a iniciar")
         break;
 }
 
-console.log("¿Cuántos plátillos quiere?")
-var cant=2;
+document.write("<h2>TICKET</h2>")
 
-console.log("Escogió", cant)
+document.write(
+"<table><tr><th>Pedido</th><th></th><th></th><th>Total</th></tr><tr><td align='center'>",comidaop,"</td><td></td><td align='center'>",cantcomida,"</td><td align='center'>$"+totalcomida,"</td></tr></table>"
+)
 
-var comida=plato*cant;
 
-console.log("Total comida $"+comida)
 
-console.log("¿Desea una bebida? SI/NO")
-var preg="SI"
-
-console.log("Escogió SI")
-
-if(preg=="SI"){
-    console.log("¿Qué desea? (escriba la letra)")
-    console.log("a) Agua Natural $15")
-    console.log("b) Soda $30")
-    console.log("c) Cerveza $45")
-    var op2="b"
-        switch(op){
-            case "a":
-                var bebida=15;
-                console.log("Escogió Agua Natural $15")
-                break;
-            case "b":
-                var bebida=30;
-                console.log("Escogió Soda $30")
-                break;
-            case "c":
-                 var bebida=45;
-                 console.log("Escogió Cerveza $45")
-                break;
-            default:
-                console.log("Opción inválida")
-                break;
-    }
-    console.log("¿Cuántas bebidas quiere?")
-    var cantb=2;
-    console.log("Escogió",cantb)
-    var bebidas=bebida*cantb;
-    console.log("Total bebida(s) $"+bebidas)
-}else{
-    bebidas=0
+/*bebidaop = prompt("Menú Bebidas \n Selecione la que sea de su agrado \n1) Agua Natural $20 \n2) Soda $30 \n3) Cerveza $45")
+switch(comidaop){
+    case "1":
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida=parseInt(cantcomida);
+        totalcomida+=40*cantcomida
+        break;
+    case "2":
+        totalcomida+=30*cantcomida
+        break;
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida=parseInt(cantcomida);
+    case "3":
+        cantcomida = prompt("¿Cuántos va a llevar?")
+        cantcomida=parseInt(cantcomida);
+        totalcomida+=25*cantcomida
+        break;
+    default:
+        alert("Opción inexistente. Refresque la pantalla para volver a iniciar")
+        break;
 }
+cantbebida = prompt("¿Cuántos va a llevar?")
+cantbebida=parseInt(cantbebida)
 
-var total=comida+bebidas
+var totalcomida=0
+var totalbebida=0
+var total=0
 
-console.log("Su total es de $"+ total)
+
+
+alert("El costo de la comida es de $"+totalcomida) */
+
